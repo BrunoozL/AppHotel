7014e273219332be061871dad09b33b30c150722
Usuario <Usuario@CPD03-M14>
2022-09-06 15:33:34 -0300

    Adicionar arquivos de projeto.

---
+++ AppHotel/AppHotel/Model/Hospedagem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AppHotel.Model
+{
+    public class Hospedagem
+    {
+        Quarto suite;
+
+        public int QntAdultos { get; set; }
+        public int QntCriancas { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+        public Quarto Suite
+        {
+            get => suite;
+
+            set
+            {
+                if (value == null)
+                    throw new Exception("Por favor, selecione uma suíte.");
+
+                suite = value;
+            }
+        }
+
+        public int Estadia
+        {
+            get
+            {
+                return DataCheckOut.Subtract(DataCheckIn).Days;
+            }
+        }
+        public double ValorTotal
+        {
+            get
+            {
+                return ((QntAdultos * Suite.ValorDiariaAdulto) + (QntCriancas * Suite.ValorDiariaCrianca)) * Estadia;
+            }
+        }
+    }
+}
---
+++ AppHotel/AppHotel/View/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppHotel.Model;
+using AppHotel.View;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppHotel.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContratacaoHospedagem : ContentPage
+    {
+        App PropriedadesApp;
+
+
+        public ContratacaoHospedagem()
+        {
+            InitializeComponent();
+
+            PropriedadesApp = (App)Application.Current;
+
+            pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+            dtcpk_checkin.MinimumDate = DateTime.Now;
+            dtcpk_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+            dtpck_checkout.MinimumDate = dtcpk_checkin.Date.AddDays(1);
+            dtpck_checkout.MaximumDate = dtcpk_checkin.Date.AddMonths(6);
+
+            //dtpck_checkout.MinimumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1);
+        }
+
+        private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DatePicker elemento = (DatePicker)sender;
+
+            DateTime data_selecionada_checkin = elemento.Date;
+
+            dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+            dtpck_checkout.MaximumDate = data_selecionada_checkin.Date.AddMonths(6);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Hospedagem h = new Hospedagem()
+                {
+                    QntAdultos = Convert.ToInt32(stp_adultos.Value),
+                    QntCriancas = Convert.ToInt32(stp_criancas.Value),
+                    Suite = (Quarto)pck_quarto.SelectedItem,
+                    DataCheckIn = dtcpk_checkin.Date,
+                    DataCheckOut = dtpck_checkout.Date
+                };
+
+                Navigation.PushAsync(new HospedagemCalculada()
+                {
+                    BindingContext = h
+                });
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
